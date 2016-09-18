f855c606bc5a874e689bfc57619df993e8875c32
yosida akiho <yoshida-akiho@outlook.jp>
2016-09-19 08:49:28 +0900

    ログイン課題

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,19 +1,21 @@
 class LoginController < ApplicationController
+  layout false
   skip_before_action :check_logined
   def index
   end
 
   def auth
-    #
-    ##
-    ##
+    #入力値がユーザー情報に存在するかどうか認証する。
+    ##存在すれば、対象のオブジェクト
+    ##存在しなければnil
     usr = User.authenticate(params[:userid],params[:password])
     if usr then
-      #
+      #セッションの初期化
       reset_session
-      #
+      #セッションに対象のユーザーのIDを保存
       session[:usr] = usr.id
-      #
+      session[:usrn] = usr.username
+      #指定のリダイレクト先へ移動
       redirect_to params[:referer]
     else
       flash.now[:referer] = params[:referer]
